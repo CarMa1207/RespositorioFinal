e7fe546ee10b25193d396faf46ef48a342e9b4ed
Adralegar <adrian.alejandre@edu.uah.es>
2024-04-28 21:17:30 +0200

    UML + clase individuo

---
+++ src/main/java/com/example/trabajofinal/Individuo/Individuo3.java
@@ -0,0 +1,7 @@
+package com.example.trabajofinal.Individuo;
+
+public class Individuo3 extends individuo{
+    public Individuo3(int vida, int porcenajereproduccion, int porcentajeclonacion, int porcentajehijo) {
+        super(vida, porcenajereproduccion, porcentajeclonacion, porcentajehijo);
+    }
+}
